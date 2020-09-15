524d5dc03ca937fa734e7075501d4a6805f804e5
Restioson <restiosondev@gmail.com>
2020-09-15 12:17:15 +0200

    bench

--- examples/crude_bench.rs
+++ examples/crude_bench.rs
@@ -1,4 +1,8 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU8, Ordering};
 use std::time::{Duration, Instant};
+
+use event_listener::Event;
 
 use xtra::prelude::*;
 use xtra::spawn::Tokio;
@@ -86,63 +90,99 @@ impl Handler<TimeReturn> for ReturnTimer {
     }
 }
 
+const COUNT: usize = 50_000_000; // May take a while on some machines
+
+async fn do_address_benchmark(name: &str, f: fn(&Address<Counter>) -> ()) {
+    let addr = Counter { count: 0 }.create(None).spawn(&mut Tokio::Global);
+
+    let start = Instant::now();
+
+    // rounding overflow
+    for _ in 0..COUNT {
+        f(&addr);
+    }
+
+    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
+    // future tokio reschedule time because of the .await
+    let total_count = addr.send(GetCount).await.unwrap();
+
+    let duration = Instant::now() - start;
+    let average_ns = duration.as_nanos() / COUNT as u128; // <120-170ns on my machine
+    println!("{} avg time of processing: {}ns", name, average_ns);
+    assert_eq!(total_count, COUNT, "total_count should equal COUNT!");
+}
+
+async fn do_parallel_address_benchmark(name: &str, workers: usize, f: fn(&Address<Counter>) -> ()) {
+    let (addr, mut ctx) = Context::new(None);
+    let start = Instant::now();
+    let done = Arc::new(AtomicU8::new(0));
+    let event = Arc::new(Event::new());
+    for _ in 0..workers {
+        tokio::spawn(ctx.attach(Counter { count: 0 }));
+        let addr = addr.clone();
+        let done = done.clone();
+        let event = event.clone();
+        tokio::task::spawn_blocking(move || {
+            for _ in 0..(COUNT / workers) {
+                f(&addr);
+            }
+            if done.fetch_add(1, Ordering::SeqCst) == (workers - 1) as u8 {
+                event.notify(1);
+            }
+        });
+    }
+
+    event.listen().await;
+
+    // rounding overflow
+    for _ in 0..COUNT - ((COUNT / workers) * workers) {
+        f(&addr);
+    }
+
+    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
+    // future tokio reschedule time because of the .await
+    let _ = addr.send(GetCount).await.unwrap();
+
+    let duration = Instant::now() - start;
+    let average_ns = duration.as_nanos() / COUNT as u128; // <120-170ns on my machine
+    println!("{} avg time of processing: {}ns", name, average_ns);
+}
+
+async fn do_channel_benchmark<F: Fn(&dyn MessageChannel<Increment>)>(name: &str, f: F) {
+    let addr = Counter { count: 0 }.create(None).spawn(&mut Tokio::Global);
+    let chan = &addr as &dyn MessageChannel<Increment>;
+
+    let start = Instant::now();
+    for _ in 0..COUNT {
+        f(chan);
+    }
+
+    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
+    // future tokio reschedule time because of the .await
+    let total_count = addr.send(GetCount).await.unwrap();
+
+    let duration = Instant::now() - start;
+    let average_ns = duration.as_nanos() / total_count as u128; // <120-170ns on my machine
+    println!("{} avg time of processing: {}ns", name, average_ns);
+    assert_eq!(total_count, COUNT, "total_count should equal COUNT!");
+}
+
+
 #[tokio::main]
 async fn main() {
-    const COUNT: usize = 50_000_000; // May take a while on some machines
-
-    /* Time do_send */
-
-    let addr = Counter { count: 0 }.create(None).spawn(&mut Tokio::Global);
-
-    let start = Instant::now();
-    for _ in 0..COUNT {
+    do_address_benchmark("address do_send", |addr| {
         let _ = addr.do_send(Increment);
-    }
+    }).await;
 
-    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
-    // future tokio reschedule time because of the .await
-    let total_count = addr.send(GetCount).await.unwrap();
+    do_parallel_address_benchmark("address do_send 2 workers", 2, |addr| {
+        let _ = addr.do_send(Increment);
+    }).await;
 
-    let duration = Instant::now() - start;
-    let average_ns = duration.as_nanos() / total_count as u128; // <140ns on my machine
-    println!("do_send avg time of processing: {}ns", average_ns);
-    assert_eq!(total_count, COUNT, "total_count should equal COUNT!");
-
-    /* Time channel do_send */
-
-    let addr = Counter { count: 0 }.create(None).spawn(&mut Tokio::Global);
-    let chan = &addr as &dyn MessageChannel<Increment>;
-
-    let start = Instant::now();
-    for _ in 0..COUNT {
+    do_channel_benchmark("channel do_send", |chan| {
         let _ = chan.do_send(Increment);
-    }
+    }).await;
 
-    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
-    // future tokio reschedule time because of the .await
-    let total_count = addr.send(GetCount).await.unwrap();
-
-    let duration = Instant::now() - start;
-    let average_ns = duration.as_nanos() / total_count as u128; // <210ns on my machine
-    println!("channel do_send avg time of processing: {}ns", average_ns);
-    assert_eq!(total_count, COUNT, "total_count should equal COUNT!");
-
-    /* Time channel send and get response */
-
-    let addr = Counter { count: 0 }.create(None).spawn(&mut Tokio::Global);
-    let chan = &addr as &dyn MessageChannel<Increment>;
-
-    let start = Instant::now();
-    for _ in 0..COUNT {
+    do_channel_benchmark("channel send", |chan| {
         let _ = chan.send(Increment);
-    }
-
-    // awaiting on GetCount will make sure all previous messages are processed first BUT introduces
-    // future tokio reschedule time because of the .await
-    let total_count = addr.send(GetCount).await.unwrap();
-
-    let duration = Instant::now() - start;
-    let average_ns = duration.as_nanos() / total_count as u128; // <210ns on my machine
-    println!("channel send avg time of processing: {}ns", average_ns);
-    assert_eq!(total_count, COUNT, "total_count should equal COUNT!");
+    }).await;
 }
