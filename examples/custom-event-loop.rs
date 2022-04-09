e2d8ba84379d5b917b21638b495991aedc4a5f9c
Thomas Eizinger <thomas@eizinger.io>
2022-04-09 13:41:59 +1000

    Fixup example

--- examples/custom-event-loop.rs
+++ examples/custom-event-loop.rs
@@ -6,7 +6,14 @@ struct Printer {
     times: usize,
 }
 
-impl Actor for Printer {}
+#[async_trait]
+impl Actor for Printer {
+    type Stop = ();
+
+    async fn stopped(self) -> Self::Stop {
+
+    }
+}
 
 struct Print(String);
 
@@ -14,7 +21,7 @@ impl Message for Print {
     type Result = ();
 }
 
-#[async_trait::async_trait]
+#[async_trait]
 impl Handler<Print> for Printer {
     async fn handle(&mut self, print: Print, _ctx: &mut Context<Self>) {
         self.times += 1;
