56b50e4d920e03e4814ebea835db55a518e888e9
Thomas Eizinger <thomas@eizinger.io>
2022-08-06 19:29:44 +0200

    Inline function in macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -6,17 +6,9 @@ use syn::{parse_quote, DeriveInput, GenericParam, WherePredicate};
 pub fn actor_derive(input: TokenStream) -> TokenStream {
     let derive_input = syn::parse::<DeriveInput>(input).expect("macro to be used as custom-derive");
 
-    actor_derive_impl(derive_input).unwrap_or_else(|e| e).into()
-}
-
-fn actor_derive_impl(
-    input: DeriveInput,
-) -> Result<proc_macro2::TokenStream, proc_macro2::TokenStream> {
-    let send_and_static_bound = send_and_static_bounds(&input);
-
-    let actor_ident = input.ident;
-    let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
-
+    let send_and_static_bound = send_and_static_bounds(&derive_input);
+    let actor_ident = derive_input.ident;
+    let (impl_generics, type_generics, where_clause) = derive_input.generics.split_for_impl();
     let where_clause = match where_clause.cloned() {
         None => parse_quote! { where #(#send_and_static_bound),* },
         Some(mut existing) => {
@@ -26,14 +18,15 @@ fn actor_derive_impl(
         }
     };
 
-    Ok(quote! {
+    quote! {
         #[xtra::prelude::async_trait]
         impl #impl_generics xtra::Actor for #actor_ident #type_generics #where_clause {
             type Stop = ();
 
             async fn stopped(self) { }
         }
-    })
+    }
+    .into()
 }
 
 /// Generics a `: Send + 'static` predicate for each type parameter present in the generics.
