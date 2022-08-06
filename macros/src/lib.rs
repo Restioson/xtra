075b3671de99e4a837c162d75c2136a021acacbf
Thomas Eizinger <thomas@eizinger.io>
2022-08-06 19:02:13 +0200

    Handle generics

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::DeriveInput;
+use syn::{parse_quote, DeriveInput, GenericParam, WherePredicate};
 
 #[proc_macro_derive(Actor)]
 pub fn actor_derive(input: TokenStream) -> TokenStream {
@@ -12,14 +12,45 @@ pub fn actor_derive(input: TokenStream) -> TokenStream {
 fn actor_derive_impl(
     input: DeriveInput,
 ) -> Result<proc_macro2::TokenStream, proc_macro2::TokenStream> {
+    let send_and_static_bound = send_and_static_bounds(&input);
+
     let actor_ident = input.ident;
+    let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
+
+    let where_clause = match where_clause.cloned() {
+        None => parse_quote! { where #(#send_and_static_bound),* },
+        Some(mut existing) => {
+            existing.predicates.extend(send_and_static_bound);
+
+            existing
+        }
+    };
 
     Ok(quote! {
         #[xtra::prelude::async_trait]
-        impl xtra::Actor for #actor_ident {
+        impl #impl_generics xtra::Actor for #actor_ident #type_generics #where_clause {
             type Stop = ();
 
             async fn stopped(self) { }
         }
     })
+}
+
+/// Generics a `: Send + 'static` predicate for each type parameter present in the generics.
+fn send_and_static_bounds(input: &DeriveInput) -> Vec<WherePredicate> {
+    input
+        .generics
+        .params
+        .iter()
+        .filter_map(|gp| match gp {
+            GenericParam::Type(tp) => Some(&tp.ident),
+            GenericParam::Lifetime(_) => None,
+            GenericParam::Const(_) => None,
+        })
+        .map(|ident| {
+            parse_quote! {
+                #ident: Send + 'static
+            }
+        })
+        .collect::<Vec<WherePredicate>>()
 }
