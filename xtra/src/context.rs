c605676787b34d3a093384fb82f09013f5ffc2ec
Thomas Eizinger <thomas@eizinger.io>
2023-09-26 16:20:39 +1000

    Reformat docs

--- xtra/src/context.rs
+++ xtra/src/context.rs
@@ -14,13 +14,14 @@ impl<A: Actor> Context<A> {
         self.running = false;
     }
 
-    /// Stop all actors on this address. This bypasses the message queue, so it will always be
-    /// handled as soon as possible by all actors, and will not wait for other messages to be
-    /// enqueued if the queue is full. In other words, it will not wait for an actor which is
-    /// lagging behind on broadcast messages to catch up before other actors can receive the shutdown
-    /// message. Therefore, each actor is guaranteed to shut down as its next action immediately
-    /// after it finishes processing its current message, or as soon as its task is woken if it is
-    /// currently idle.
+    /// Stop all actors on this address.
+    ///
+    /// This bypasses the message queue, so it will always be handled as soon as possible by all actors.
+    /// It will not wait for other messages to be enqueued if the queue is full.
+    /// In other words, it will not wait for an actor which is lagging behind on broadcast messages
+    /// to catch up before other actors can receive the shutdown message.
+    /// Therefore, each actor is guaranteed to shut down as its next action immediately after it
+    /// finishes processing its current message, or as soon as its task is woken if it is currently idle.
     ///
     /// This is similar to calling [`Context::stop_self`] on all actors active on this address, but
     /// a broadcast message that would cause [`Context::stop_self`] to be called may have to wait
